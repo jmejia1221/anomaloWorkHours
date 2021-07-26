0087bcae7a7c3ea8c6dd46845506c4ec32b8479e
John Mejia <jhondario.09@gmail.com>
2021-07-25 21:10:57 -0500

    Added new Spinner component

---
+++ src/Components/UI/Spinner/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// CSS
+import './Spinner.css';
+
+const Spinner = () => {
+    return (
+        <div className="loader"></div>
+    );
+};
+
+export default Spinner;
